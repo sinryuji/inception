18789511ee105647e4a7c4c882d576192d88fd3c
sinryuji <siler2474@gmail.com>
2023-01-29 03:01:35 +0900

    ing

---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,22 @@
+wp core download --allow-root
+
+until mysqladmin -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASSWORD ping 2> /dev/null;
+do
+	echo "MariaDB is not ready...."
+	sleep 1
+done
+
+wp config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
+
+wp core install --url=localhost/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+
+echo "\
+--------------------
+
+@wordpress ready
+
+--------------------"
+
+exec /usr/sbin/php-fpm7.3 -F
